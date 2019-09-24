428c525fc2c959cdff6e4f104dfd92d1d999a9cc
StIfleR13 <savenko.vadym@gmail.com>
2019-09-24 16:52:12 +0300

    Проверка
    первый раз что-то меняю

---
+++ Labs/Labs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Hello world";
+            Console.WriteLine(msg);
+        }
+    }
+}
