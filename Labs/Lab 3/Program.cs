d0461af8ccc254e4c8fcd1810a4c7792caa3ac18
StIfleR13 <savenko.vadym@gmail.com>
2019-09-24 17:48:01 +0300

    Lab 3
    3. Вариант
    Перевірити істинність вислову: Дані числа x, у є координатами
    точки, що лежить у першому або третьому квадранті&quot;.

---
+++ Labs/Lab 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Enter x");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y");
+            y = Convert.ToDouble(Console.ReadLine());
+            if ((x < 0 && y < 0) || (x > 0 && y > 0))
+            {
+                Console.WriteLine("This point in 1-st or 3-d part");
+            }
+            else
+            {
+                Console.WriteLine("This point isnt in 1 - st or 3 - d part");
+            }
+            Console.ReadLine();
+        }
+    }
+}
