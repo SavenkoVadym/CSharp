70cdd0db7ab8af81cc1d4b6ca2fc974a50907003
StIfleR13 <savenko.vadym@gmail.com>
2019-10-15 21:13:25 +0300

    Lab 7
    Правильно путь указан для разных проектов

---
+++ Labs/Lab7/Lab 7.6.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab_7._6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static string Method_Recursion(char[] arr, char[] arrReverse, int i, int j)
+        {
+            arrReverse[j] = arr[i - 1];
+            if (arrReverse[arr.Length - 1] == '\0')
+            {
+                return Method_Recursion(arr, arrReverse, i - 1, j + 1);
+            }
+            else
+            {
+                string revers_string = new string(arrReverse);
+                return revers_string;
+            }
+        }
+    }
+}
---
+++ Labs/Lab7/Lab 7.8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_7._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
