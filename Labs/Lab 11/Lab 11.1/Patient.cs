76333b846fbda3f2d794a8398be3ca58c8e68740
savenko1vadym <savenko.vadym@gmail.com>
2019-12-23 17:45:07 +0200

    Lab 11
    Lab 11 и случайно поменял название папки в которой хранились 7.n лабы поэтому загрузил заново

---
+++ Labs/Lab 11/Lab 11.1/Patient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Lab_11._1
+{
+    class Patient
+    {
+        public Patient()
+        {
+            MRTsuccessful = false;
+            KTsuccessful = false;
+            XRaySuccessful = false;
+            LORsuccessful = false;
+            UZIsuccessful = false;
+            RevmoSuccessful = false;
+        }
+        public bool MRTsuccessful { get; set; }
+        public bool KTsuccessful { get; set; }
+        public bool XRaySuccessful { get; set; }
+        public bool LORsuccessful { get; set; }
+        public bool UZIsuccessful { get; set; }
+        public bool RevmoSuccessful { get; set; }
+        public void DisplayStatus()
+        {
+            Console.WriteLine("Patient status:");
+            Console.WriteLine($"MRT = { MRTsuccessful}\nKT = {KTsuccessful}\nXray = {XRaySuccessful}\nLOR = {LORsuccessful}\nUZI = {UZIsuccessful}\nRevmo = {RevmoSuccessful}\n");
+        }
+    }
+}
---
+++ Labs/Lab 11/Lab 11.2/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_11._2
+{
+    public delegate bool StudentPredicateDelegate(Student student);
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Student() { }
+        public Student(string LastName, string FirstName, int Age)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Age = Age;
+        }
+        public static bool AgeMore18(Student student)
+        {
+            return (student.Age >= 18 ? true : false);
+        }
+        public static bool IsFirstLetterA(Student student)
+        {
+            return (student.FirstName.IndexOf("A") == 0 ? true : false);
+        }
+        public static bool IsLastNameMoreThanThreeSymbols(Student student)
+        {
+            return (student.LastName.Length > 3 ? true : false);
+        }
+    }
+}
